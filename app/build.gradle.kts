c8834a2a5e8622c3c6a95f444b90707d22ddd2b5
jose.hernandez <jose.hernandez99@grupobimbo.com>
2026-03-26 14:01:41 -0600

    version         versionName = "1.2"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     defaultConfig {
         applicationId = "com.linkersconsulting.appaint"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
